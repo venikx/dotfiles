491fd8d64bf4d4a9c3932650a70f19f0d084b275
Kevin Rangel <code@venikx.com>
2020-01-27 01:30:33 +0200

    [emacs] Remove js2-mode and typescript-mode depedency
    These dependencies doesn't seem needed anymore to properly work with jsx
    files. js-mode has improved in Emacs 27 and beyond.
    
    It might be worth checking out lsp-mode in favor of tide, since
    lsp supposedly had some performance increase with Emacs 27+.

--- .emacs.d/to-be-dumped.el
+++ .emacs.d/to-be-dumped.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (nginx-mode dockerfile-mode toml-mode yasnippet-snippets yaml-mode which-key web-mode use-package tide smart-mode-line ranger rainbow-mode racer prettier-js origami org-pomodoro org-plus-contrib org-bullets nlinum-relative ledger-mode json-mode js2-mode ivy-rich highlight-operators highlight-numbers highlight-escape-sequences general focus flycheck-rust exec-path-from-shell evil-surround evil-magit evil-collection emmet-mode dimmer counsel-projectile company cargo add-node-modules-path))))
+   '(nginx-mode dockerfile-mode toml-mode yasnippet-snippets yaml-mode which-key web-mode use-package tide smart-mode-line ranger rainbow-mode racer prettier-js origami org-pomodoro org-plus-contrib org-bullets nlinum-relative ledger-mode json-mode js2-mode ivy-rich highlight-operators highlight-numbers highlight-escape-sequences general focus flycheck-rust exec-path-from-shell evil-surround evil-magit evil-collection emmet-mode dimmer counsel-projectile company cargo add-node-modules-path)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
