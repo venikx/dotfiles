503652ad7060f4b7232626abd028083e3c499e88
Kevin Rangel <code@venikx.com>
2020-01-28 02:40:12 +0200

    [emacs] Opem magit status in a fullscreen

--- .emacs.d/to-be-dumped.el
+++ .emacs.d/to-be-dumped.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d5d2ab76985738c142adbe6a35dc51c8d15baf612fdf6745c901856457650314" default))
  '(package-selected-packages
    '(nginx-mode dockerfile-mode toml-mode yasnippet-snippets yaml-mode which-key web-mode use-package tide smart-mode-line ranger rainbow-mode racer prettier-js origami org-pomodoro org-plus-contrib org-bullets nlinum-relative ledger-mode json-mode js2-mode ivy-rich highlight-operators highlight-numbers highlight-escape-sequences general focus flycheck-rust exec-path-from-shell evil-surround evil-magit evil-collection emmet-mode dimmer counsel-projectile company cargo add-node-modules-path)))
 (custom-set-faces
