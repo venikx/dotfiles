c1a8da0bd80809a28ca412ada6d3ed7337b274da
Kevin Rangel <code@venikx.com>
2020-05-07 19:51:12 +0200

    [yabai] Add "i3" to MacOS

---
+++ .config/skhd/open-or-create-space.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if ! yabai -m query --spaces --space $@; then
+    yabai -m space --create
+    sidx="$(yabai -m query --spaces | jq -r 'map(select(."native-fullscreen" == 0))[-1].index')"
+    yabai -m space "${sidx}" --label $@
+fi
+
+# if yabai -m query --windows --space | jq -e '. == []'; then
+#    yabai -m space --destroy
+# fi
+
+yabai -m space --focus $@
