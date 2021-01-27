f0510e5e5c35c362bf589fbb4c1f82fe80900677
Kevin Rangel <code@venikx.com>
2021-01-27 15:09:52 +0200

    Clean up the $HOME directory by forcing to different XDG location

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -12,26 +12,19 @@
       XDG_DATA_HOME   = "$HOME/.local/share";
       XDG_BIN_HOME    = "$HOME/.local/bin";
     };
-   # variables = {
+   variables = {
       # Conform more programs to XDG conventions. The rest are handled by their
       # respective modules.
-   #    __GL_SHADER_DISK_CACHE_PATH = "$XDG_CACHE_HOME/nv";
-   #    ASPELL_CONF = ''
-   #      per-conf $XDG_CONFIG_HOME/aspell/aspell.conf;
-   #      personal $XDG_CONFIG_HOME/aspell/en_US.pws;
-   #      repl $XDG_CONFIG_HOME/aspell/en.prepl;
-   #    '';
-   #    CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
-   #    HISTFILE        = "$XDG_DATA_HOME/bash/history";
-   #    INPUTRC         = "$XDG_CONFIG_HOME/readline/inputrc";
-   #    LESSHISTFILE    = "$XDG_CACHE_HOME/lesshst";
-   #    WGETRC          = "$XDG_CONFIG_HOME/wgetrc";
-   # };
+      HISTFILE        = "$XDG_DATA_HOME/bash/history";
+      INPUTRC         = "$XDG_CONFIG_HOME/readline/inputrc";
+      LESSHISTFILE    = "$XDG_CACHE_HOME/lesshst";
+      WGETRC          = "$XDG_CONFIG_HOME/wgetrc";
+   };
 
-    # Move ~/.Xauthority out of $HOME (setting XAUTHORITY early isn't enough)
-   #  extraInit = ''
-   #    export XAUTHORITY=/tmp/Xauthority
-   #    [ -e ~/.Xauthority ] && mv -f ~/.Xauthority "$XAUTHORITY"
-   #  '';
+   # Move ~/.Xauthority out of $HOME (setting XAUTHORITY early isn't enough)
+   extraInit = ''
+     export XAUTHORITY=/tmp/Xauthority
+     [ -e ~/.Xauthority ] && mv -f ~/.Xauthority "$XAUTHORITY"
+   '';
   };
 }
