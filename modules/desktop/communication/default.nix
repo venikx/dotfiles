370d56c64758c36e45f582c22ed5303d07718684
Kevin Rangel <code@venikx.com>
2021-03-26 21:27:37 +0200

    [nixos] Use stable pkg repos for discord binary

--- modules/desktop/communication/default.nix
+++ modules/desktop/communication/default.nix
@@ -3,7 +3,7 @@
 with lib;
 let
   cfg = config.modules.desktop.communication;
-  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  # unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
   options.modules.desktop.communication = with types; {
     discord.enable = mkOption {
@@ -25,9 +25,7 @@ in {
   config = {
     home-manager.users.venikx = {
       home.packages = with pkgs; [
-        # If not installed from the bleeding edge, Discord will sometimes
-        # soft-lock itself on a "there's an update for discord" screen.
-        (mkIf cfg.discord.enable unstable.discord)
+        (mkIf cfg.discord.enable discord)
         (mkIf cfg.slack.enable slack)
         (mkIf cfg.teams.enable teams)
       ];
