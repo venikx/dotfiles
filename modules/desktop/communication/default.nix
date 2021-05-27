520419693547104be6e4a6b11b08aa5429ee684f
Kevin Rangel <code@venikx.com>
2021-05-27 16:12:41 +0300

    Disable discord temporily, malfunctioning package

--- modules/desktop/communication/default.nix
+++ modules/desktop/communication/default.nix
@@ -3,9 +3,10 @@
 with lib;
 let
   cfg = config.modules.desktop.communication;
-  # unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
   options.modules.desktop.communication = with types; {
+
     discord.enable = mkOption {
       type = bool;
       default = false;
@@ -25,7 +26,7 @@ in {
   config = {
     home-manager.users.venikx = {
       home.packages = with pkgs; [
-        (mkIf cfg.discord.enable discord)
+        #(mkIf cfg.discord.enable unstable.discord)
         (mkIf cfg.slack.enable slack)
         (mkIf cfg.teams.enable teams)
       ];
