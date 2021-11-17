898933f4f39dbe6827f414ec17f9f4e3c1886128
Kevin Rangel <code@venikx.com>
2021-11-17 16:41:36 +0900

    Use temporary single monitor with lower resolution

--- modules/hardware/monitors.nix
+++ modules/hardware/monitors.nix
@@ -17,26 +17,26 @@ in {
         "default" = {
           fingerprint = {
             DP-4 = "*";
-            HDMI-0 = "*";
+            # HDMI-0 = "*";
           };
           config = {
             DP-4 = {
               enable = true;
               primary = true;
-              rate = "144.00";
-              mode = "2560x1440";
+              rate = "59.95";
+              mode = "1920x1200";
               dpi = 100;
               position = "0x0";
             };
-            HDMI-0 = {
-              enable = true;
-              primary = false;
-              mode = "1920x1080";
-              rate = "60.00";
-              rotate = "right";
-              dpi = 100;
-              position = "2561x0";
-            };
+            # HDMI-0 = {
+            #   enable = true;
+            #   primary = false;
+            #   mode = "1920x1080";
+            #   rate = "60.00";
+            #   rotate = "right";
+            #   dpi = 100;
+            #   position = "2561x0";
+            # };
           };
         };
       };
