af6bf5b58d92ddf78a687d992217ad3b888e4220
Kevin Rangel <code@venikx.com>
2021-12-02 20:45:58 +0900

    Move venikx.com publishing to doom config
    Why? The publishing feature holds the current emacs configuration in
    consideration to htmlize the source code blocks. In the future I wonder
    if it's possible to add documentation of the keywords as popup-hover.

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -55,3 +55,6 @@
 (unpin! org-roam)
 (package! websocket)
 (package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
+
+;; Publishing Websites
+(package! esxml)
