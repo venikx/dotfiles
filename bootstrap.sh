b8743916a8661d24043ec5f88c1ab9cb80d3f589
Kevin Rangel <code@venikx.com>
2022-03-07 03:19:51 +0200

    chore(nix): update nixos to 21.11

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
-nix-channel --add https://nixos.org/channels/nixos-21.05 nixos
-nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-nix-channel --update
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
+sudo nix-channel --add https://nixos.org/channels/nixos-21.11 nixos
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+sudo nix-channel --update
