76abf1bafc9e7fb64e2ec19bacd0a01498645b1e
Kevin Rangel <code@venikx.com>
2022-09-09 04:23:47 +0200

    feat: handle gnupg and yubikeys with nix-darwin

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, options, ... }:
 
+#TODO(Kevin): It might be better to seperate this file completely between Linux and Mac
+# The home-manager only is capable of configuring systemd services. Therefore the gpg-agent
+# can't be configured using home-manager. Not sure if we should separate, or just handle it
+# for both systems
 with lib;
 let
   cfg = config.modules.shell.gnupg;
@@ -12,33 +16,53 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.variables.GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
-    environment.shellInit = ''
-      gpg-connect-agent /bye
-      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-    '';
+    #TODO(Kevin): These are specific to Linux and need to be checked if these really are needed
+    #services.pcscd.enable = true;
+    #services.udev.packages = [ pkgs.yubikey-personalization ];
 
-    services.pcscd.enable = true;
-    services.udev.packages = [ pkgs.yubikey-personalization ];
-
-    programs.ssh.startAgent = false;
     programs.gnupg.agent = {
       enable = true;
-      enableSSHSupport = true;
+    #  enableSSHSupport = true;
     };
 
+    #TODO(Kevin): Extract out as these are yubikey specific and not pure gpg
+    #TODO(Kevin): Why do I need these?
+    homebrew.brews = [ "pinentry-mac"];
+
     home-manager.users.venikx = {
-      home.packages = with pkgs; [
-        yubikey-manager-qt
-        yubikey-personalization
-      ];
-      
+      home = {
+        #TODO(Kevin): Extract out as these are yubikey specific and not pure gpg
+        packages = with pkgs; [
+          yubikey-manager
+          yubikey-personalization
+        ];
+        sessionVariables = {
+          GNUPGHOME = "${config.environment.variables.XDG_CONFIG_HOME}/gnupg";
+        };
+      };
+
+      programs.zsh.initExtra = ''
+      gpg-connect-agent /bye
+      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+      gpgconf --launch gpg-agent
+    '';
+
+      programs.gpg = {
+        enable = true;
+        homedir = "${config.users.users.venikx.home}/.config/gnupg";
+        scdaemonSettings = {
+          disable-ccid = true;
+        };
+      };
+
+      #TODO(Kevin): properly separate between macos and linux
       xdg.configFile."gnupg/gpg-agent.conf" = {
         text = ''
+          homedir ${config.environment.variables.XDG_CONFIG_HOME}/gnupg
           enable-ssh-support
           default-cache-ttl 300
           max-cache-ttl 3600
-          pinentry-program ${pkgs.pinentry.qt}/bin/pinentry
+          pinentry-program /opt/homebrew/bin/pinentry-mac
         '';
       };
     };
