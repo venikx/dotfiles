77b5ddb2b52b6442b0b76cbddd326bca4dcac97b
Kevin Rangel <code@venikx.com>
2022-09-09 03:17:33 +0200

    refactor(xdg): move variables inside home-manager

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,42 +1,55 @@
-{ config, home-manager, ... }:
+{ config, lib, pkgs, home-manager, ... }:
+
+with lib;
+
 {
-  home-manager.users.venikx.xdg = {
-    enable = true;
-    userDirs.enable = true;
-    userDirs.download = "$HOME/dl";
-    userDirs.documents = "$HOME/docs";
-    userDirs.pictures = "$HOME/media/pics";
-    userDirs.music = "$HOME/media/music";
-    userDirs.videos = "$HOME/media/videos";
-    userDirs.desktop = "$HOME/system/desktop";
-    userDirs.publicShare = "$HOME/system/public";
-    userDirs.templates = "$HOME/system/templates";
+  environment = {
+   variables = {
+     XDG_CACHE_HOME  = "$HOME/.cache";
+     XDG_CONFIG_HOME = "$HOME/.config";
+     XDG_DATA_HOME   = "$HOME/.local/share";
+     XDG_BIN_HOME    = "$HOME/.local/bin";
+   };
   };
 
-  environment = {
-    sessionVariables = {
-      # These are the defaults, and xdg.enable does set them, but due to load
-      # order, they're not set before environment.variables are set, which could
-      # cause race conditions.
-      XDG_CACHE_HOME  = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME   = "$HOME/.local/share";
-      XDG_BIN_HOME    = "$HOME/.local/bin";
-    };
-   variables = {
-      # Conform more programs to XDG conventions. The rest are handled by their
-      # respective modules.
-      HISTFILE        = "$XDG_DATA_HOME/bash/history";
-      INPUTRC         = "$XDG_CONFIG_HOME/readline/inputrc";
-      LESSKEY         = "$XDG_CACHE_HOME/less/lesskey";
-      LESSHISTFILE    = "$XDG_CACHE_HOME/less/history";
-      WGETRC          = "$XDG_CONFIG_HOME/wgetrc";
-   };
+  home-manager.users.venikx = {
+    programs.zsh.enable = true;
+    xdg = mkMerge([
+      {
+        enable = true;
+        # home-manager expects these to be paths, and $HOME isn't
+        # cutting for current unknown reasons
+        configHome = "${config.users.users.venikx.home}/.config";
+        dataHome = "${config.users.users.venikx.home}/.local/share";
+        cacheHome  = "${config.users.users.venikx.home}/.cache";
+      }
+      (mkIf pkgs.stdenv.isLinux {
+        userDirs.enable = true;
+        userDirs.download = "$HOME/dl";
+        userDirs.documents = "$HOME/dl/docs";
+        userDirs.pictures = "$HOME/media/pics";
+        userDirs.music = "$HOME/media/music";
+        userDirs.videos = "$HOME/media/videos";
+        userDirs.desktop = "$HOME/system/desktop";
+        userDirs.publicShare = "$HOME/system/public";
+        userDirs.templates = "$HOME/system/templates";
+      })
+    ]);
 
-   # Move ~/.Xauthority out of $HOME (setting XAUTHORITY early isn't enough)
-   extraInit = ''
-     export XAUTHORITY=/tmp/Xauthority
-     [ -e ~/.Xauthority ] && mv -f ~/.Xauthority "$XAUTHORITY"
-   '';
+    home = {
+      homeDirectory = config.users.users.venikx.home;
+      username = config.users.users.venikx.name;
+      sessionPath = [ config.environment.variables.XDG_CACHE_HOME ];
+
+      sessionVariables = {
+        # Conform more programs to XDG conventions. The rest are handled by their
+        # respective modules.
+        HISTFILE        = "${config.environment.variables.XDG_DATA_HOME}/bash/history";
+        INPUTRC         = "${config.environment.variables.XDG_CONFIG_HOME}/readline/inputrc";
+        LESSKEY         = "${config.environment.variables.XDG_CACHE_HOME}/less/lesskey";
+        LESSHISTFILE    = "${config.environment.variables.XDG_CACHE_HOME}/less/history";
+        WGETRC          = "${config.environment.variables.XDG_CONFIG_HOME}/wgetrc";
+      };
+    };
   };
 }
