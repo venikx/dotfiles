22a74b2fd658471b83095a1fe9e2957df99a952b
Kevin Rangel <code@venikx.com>
2022-09-16 21:27:38 +0300

    refactor: move sxhkd to nix config

--- modules/desktop/bspwm.nix
+++ modules/desktop/bspwm.nix
@@ -35,7 +35,6 @@ in {
       redshift.enable = true;
       xserver = {
         enable = true;
-        # TODO(kevin): Part of the device specific configuration as desktops don't have touchpads
         libinput.enable = true;
         displayManager = {
           defaultSession = "none+bspwm";
@@ -57,8 +56,42 @@ in {
     };
 
     home-manager.users.venikx = {
+      services.sxhkd = {
+        enable = true;
+        keybindings = {
+          # Shutting down the system
+          "super + shift + x" = "prompt 'Shuwdown computer?' 'shutdown -h now'";
+          "super + shift + BackSpace" = "prompt 'Reboot computer?' 'reboot'";
+          "super + shift + Escape" = "prompt 'Leave Xorg?' 'killall Xorg'";
+          # Restarts bspwm (most when testing out configurations)
+          "super + shift + r" = "bspc wm -r";
+          "super + {_,shift + }q" = "bspc node -{c,k}";
+
+          # Opening common commands
+          "super + Return" = "$TERMINAL";
+          "super + d" = "dmenu_run";
+          "super + w" = "$BROWSER";
+
+          # Moving around windows
+          "super + {_,shift +}{1-9,0}" = "bspc {desktop -f, node -d} {1-9,10};";
+          "super + {h,j,k,l}" = "bspc node -f {west,south,north,east}";
+          "super + shift + {h,j,k,l}" = "bspc node -s {west,south,north,east}";
+          "super + {_,ctrl + }f" = "bspc node -t ~{fullscreen,floating}";
+          "super + space" = "bspc node -s biggest.local || bspc node -s next.local"; # swap windows
+
+          # TODO(Kevin): Media Keys
+          "Print" = "scrcap";
+          "XF86MonBrightnessUp" = "light -A 5";
+          "XF86MonBrightnessDown" = "light -U 5";
+          "XF86AudioMute" = "amixer -q set Master toggle";
+          "XF86AudioLowerVolume" = "amixer -q set Master 10%- unmute";
+          "XF86AudioRaiseVolume" = "amixer -q set Master 10%+ unmute";
+          "XF86Audio{Play,Pause}" = "spt-send toggle";
+          "XF86AudioNext" = "spt-send next";
+          "XF86AudioPrev" = "spt-send prev";
+        };
+      };
       xdg.configFile = {
-        "sxhkd".source = "${configDir}/sxhkd";
         "bspwm" = {
           source = "${configDir}/bspwm";
           # link recursively so other modules can link files in their folders
