a5d92d04330e8d10b10f43de56b796539e41d2f9
Kevin Rangel <code@venikx.com>
2022-09-17 20:20:40 +0300

    refactor: move nix.nix into common.nix

--- modules/common.nix
+++ modules/common.nix
@@ -1,7 +1,50 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./home-manager
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableBashCompletion = true;
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      # usefull shell stuff
+      bat
+      fzf
+      ripgrep
+
+      # extracting file and dirs
+      unzip
+      zip
+      ranger
+
+      # standard toolset
+      vim
+      coreutils-full
+      wget
+      curl
+      git
+    ];
+
+    shells = with pkgs; [ bash zsh fish ];
+  };
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    gc.automatic = true;
+  };
+  nixpkgs.config.allowUnfree = true;
+
+  # can I remove this?
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.0.2u"
   ];
 }
