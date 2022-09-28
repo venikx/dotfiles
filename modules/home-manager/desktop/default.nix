537cf8df5c139053d4207f50d9aa7820c3b046e4
Kevin Rangel <code@venikx.com>
2022-09-28 03:38:30 +0300

    feat: add davinci resolve video editing

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -2,5 +2,6 @@
 {
   imports = [
     ./terminal.nix
+    ./media.nix
   ];
 }
---
+++ modules/home-manager/desktop/media.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # video editing
+    davinci-resolve # only mov is supported, maybe this is fixed in v18?
+    ffmpeg_5
+  ];
+}
