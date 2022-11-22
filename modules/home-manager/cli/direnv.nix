90e841e49159b8d69cafd72c272dec5dee18d458
Kevin Rangel <code@venikx.com>
2022-11-22 04:53:09 +0200

    feat: add whitelisted directory for direnv

--- modules/home-manager/cli/direnv.nix
+++ modules/home-manager/cli/direnv.nix
@@ -1,10 +1,16 @@
-{ ... }:
+{ config, ... }:
 {
   programs = {
     direnv = {
       enable = true;
       nix-direnv.enable = true;
       enableZshIntegration = true;
+      config = {
+        whitelist = {
+          prefix = [
+            "${config.home.homeDirectory}/code" ];
+        };
+      };
     };
 
     git.ignores = [ ".direnv" ];
