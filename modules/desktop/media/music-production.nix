cd1953e015bc78576c866ae317c3c208d1212d10
Kevin Rangel <code@venikx.com>
2023-03-01 05:14:10 +0200

    chore: cleanup completed todos

--- modules/desktop/media/music-production.nix
+++ modules/desktop/media/music-production.nix
@@ -15,14 +15,10 @@ in {
       home.packages = with pkgs; [
         reaper # full-featured DAW
         # ardour # FOSS DAW, can't run executable
-        lmms # midi and for 8bit music (for now)
+        # lmms # midi and for 8bit music (for now)
         # zrythm # Modern alternative to lmms
         # renoise # tracker, but unstable
       ];
-      # TODO(Kevin): Currently not possible for lmms to define a different
-      # configuration folder, so .lmmsrc.xml will be recreated in the home
-      # folder. However the lmms folder itself (containing plugins etc) is
-      # created in XDG_DOCUMENTS_DIR
     };
   };
 }
