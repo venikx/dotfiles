726e1ede43fbe876bddcdfe08670625139a77058
Kevin Rangel <code@venikx.com>
2023-11-17 01:41:45 +0200

    feat(syncthing): sync between air and earth

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -12,22 +12,6 @@
       configDir = "/home/venikx/docs/syncthing/.config";
       overrideDevices = true;
       overrideFolders = true;
-      settings = {
-        devices = {
-          "kevin-iphone" = {
-            id =
-              "ELIRVGI-W7IUR4D-HRK25JP-HML6LPG-QEH76SY-BTAL4Q6-YO66RTD-SOP37AL";
-          };
-        };
-        folders = {
-          "org" = { # Name of folder in Syncthing, also the folder ID
-            path = "/home/venikx/org/gtd"; # Which folder to add to Syncthing
-            devices =
-              [ "kevin-iphone" ]; # Which devices to share the folder with
-          };
-        };
-      };
     };
-
   };
 }
