8756451f8d90d2cdbaf20af6c685dcf4a9a8d5bf
Kevin Rangel <code@venikx.com>
2024-02-22 20:46:23 +0200

    chore: formatting

--- modules/nixos/gnupg.nix
+++ modules/nixos/gnupg.nix
@@ -1,12 +1,11 @@
 { config, lib, pkgs, options, ... }:
 
-with lib;
-{
+with lib; {
   environment.variables.GNUPGHOME = "$HOME/.config/gnupg";
   environment.shellInit = ''
-      gpg-connect-agent /bye
-      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-    '';
+    gpg-connect-agent /bye
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  '';
 
   services.pcscd.enable = true;
   services.udev.packages = [ pkgs.yubikey-personalization ];
@@ -25,18 +24,15 @@ with lib;
       };
     };
 
-    home.packages = with pkgs; [
-      yubikey-manager-qt
-      yubikey-personalization
-    ];
+    home.packages = with pkgs; [ yubikey-manager-qt yubikey-personalization ];
 
     xdg.configFile."gnupg/gpg-agent.conf" = {
       text = ''
-          enable-ssh-support
-          default-cache-ttl 300
-          max-cache-ttl 3600
-          pinentry-program ${pkgs.pinentry.qt}/bin/pinentry
-        '';
+        enable-ssh-support
+        default-cache-ttl 300
+        max-cache-ttl 3600
+        pinentry-program ${pkgs.pinentry.qt}/bin/pinentry
+      '';
     };
   };
 }
