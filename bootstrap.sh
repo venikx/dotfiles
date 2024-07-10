8e417dda73ecd65d10ac03ee169be251dbbce8f4
Kevin Rangel <code@venikx.com>
2024-07-10 13:31:03 +0300

    chore: remove unused script

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
-sudo nix-channel --add https://nixos.org/channels/nixos-22.05 nixos
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-sudo nix-channel --update
