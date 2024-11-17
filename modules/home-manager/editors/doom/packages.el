3f9c5be54358fde6a219fa1ec5402a1ade08831a
Kevin Rangel <code@venikx.com>
2024-11-17 20:57:43 +0100

    emacs: add gptel

--- modules/home-manager/editors/doom/packages.el
+++ modules/home-manager/editors/doom/packages.el
@@ -60,6 +60,7 @@
 (package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
 (package! elfeed-protocol)
 (package! osm)
+(package! gptel)
 
 ;; Publishing Websites
 (package! esxml)
