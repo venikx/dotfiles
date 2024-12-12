c04f63c657a62c24351f5afb4ba767fccdad4613
Kevin Rangel <code@venikx.com>
2024-12-12 21:26:35 +0200

    feat: add smb documents file share

--- hosts/x86_64-linux/earth/default.nix
+++ hosts/x86_64-linux/earth/default.nix
@@ -48,4 +48,38 @@
     };
     folders = { "org" = { devices = [ "air-nixos" ]; }; };
   };
+
+  fileSystems = let nasIP = "172.19.20.10";
+  in {
+    "/" = {
+      device = "/dev/disk/by-uuid/4df3f4a4-fac6-471c-a8e1-11c03c2c093c";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/C831-07CF";
+      fsType = "vfat";
+    };
+
+    "/mnt/nas/entertainment" = {
+      device = "${nasIP}:/mnt/tank/entertainment";
+      fsType = "nfs";
+      options = [ "x-systemd.automount" "noauto" ];
+    };
+
+    "/mnt/nas/documents" = {
+      device = "//${nasIP}/documents";
+      fsType = "cifs";
+      options = [
+        "credentials=/etc/nixos/smb-secrets"
+        "vers=3.0"
+        "x-systemd.automount"
+        "x-systemd.requires=network-online.target"
+        "x-systemd.after=network-online.target"
+        "uid=${toString config.users.users.syncthing.uid}"
+        "gid=${toString config.users.groups.syncthing.gid}"
+      ];
+    };
+  };
+
 }
--- hosts/x86_64-linux/earth/hardware-configuration.nix
+++ hosts/x86_64-linux/earth/hardware-configuration.nix
@@ -11,28 +11,10 @@
     kernelModules = [ "kvm-amd" ];
     kernelParams = [ ];
     kernelPackages = pkgs.linuxPackages_latest;
-    loader.grub.configurationLimit = 3;
   };
 
   # Are these really needed? Seems so atm.
   hardware.enableRedistributableFirmware = true;
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/4df3f4a4-fac6-471c-a8e1-11c03c2c093c";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C831-07CF";
-    fsType = "vfat";
-  };
-
-  fileSystems."/mnt/nas/entertainment" = {
-    device = "172.19.20.10:/mnt/tank/entertainment";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" ];
-  };
-
   swapDevices = [ ];
-
 }
