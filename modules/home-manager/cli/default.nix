3e7e9669d69c6a3c88cd68290303fd2bfec67979
Kevin De Baerdemaeker <code@venikx.com>
2025-11-22 16:04:53 +0200

    feat: install claude-code

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -1,11 +1,17 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
-  imports =
-    [
-      ./direnv.nix
-      ./git.nix
-      ./ssh.nix
-      ./zsh.nix
+  imports = [
+    ./direnv.nix
+    ./git.nix
+    ./ssh.nix
+    ./zsh.nix
+  ];
+
+  home.packages =
+    with pkgs;
+    [ ]
+    ++ lib.optionals stdenv.isLinux [
+      claude-code
     ];
 }
