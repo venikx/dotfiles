2d43bcb38ef337b7658465ff4b113409becccb27
Kevin De Baerdemaeker <code@venikx.com>
2026-02-23 21:14:15 +0200

    feat(bash): create define script to look up in dictionary

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -1,4 +1,5 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+{
   imports = [
     ./browsers.nix
     ./desktop-environment.nix
@@ -46,9 +47,53 @@
     calibre
     mpv-unwrapped
     vlc # in case mpv doesn't work
+    # scripts
     (pkgs.writeShellScriptBin "scrcap" ''
       ${pkgs.maim}/bin/maim -s | ${pkgs.xclip}/bin/xclip -selection clipboard -t image/png
     '')
-  ];
+    (pkgs.writeShellScriptBin "define" ''
+      if [ $# -gt 0 ]; then
+        word="$*"
+      else
+        word=$(${pkgs.xclip}/bin/xclip -o -selection primary 2>/dev/null || ${pkgs.wl-clipboard}/bin/wl-paste 2>/dev/null)
+      fi
 
+      if [ -z "$word" ] || echo "$word" | grep -q '/'; then
+        ${pkgs.libnotify}/bin/notify-send -h string:bgcolor:#bf616a -t 2000 "Invalid input."
+        exit 0
+      fi
+
+      LANGS="en nl fi"
+      lang=$(printf "%s\n" $LANGS | dmenu -c -i -l 5 -p "language")
+      [ -z "$lang" ] && exit 0
+
+      urlencode() {
+        echo -n "$1" | ${pkgs.jq}/bin/jq -s -R -r @uri
+      }
+      encoded_word=$(urlencode "$word")
+
+      query=$(${pkgs.curl}/bin/curl -s --connect-timeout 5 --max-time 10 "https://freedictionaryapi.com/api/v1/entries/$lang/$encoded_word")
+      if [ $? -ne 0 ] || [ -z "$query" ]; then
+        ${pkgs.libnotify}/bin/notify-send -h string:bgcolor:#bf616a -t 3000 "Connection error."
+        exit 1
+      fi
+
+      if echo "$query" | ${pkgs.jq}/bin/jq -e '.entries == []' > /dev/n -cull; then
+        ${pkgs.libnotify}/bin/notify-send -h string:bgcolor:#bf616a -t 3000 "No definitions found."
+        exit 0
+      fi
+
+      def=$(echo "$query" | ${pkgs.jq}/bin/jq -r '
+        .entries
+        | group_by(.partOfSpeech)[]
+        | .[0].partOfSpeech as $pos
+        | "\($pos):\n" +
+          (map(
+             (.senses[0:2] // []) | map("  - " + (.definition // "")) | join("\n")
+           ) | join("\n")) + "\n"
+      ')
+
+      ${pkgs.libnotify}/bin/notify-send -t 60000 "$word" "$def"
+    '')
+  ];
 }
