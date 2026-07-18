91d50f043cc90b2f44ed99221b2936a7ef7a6862
Kevin De Baerdemaeker <code@venikx.com>
2026-07-18 16:14:33 +0300

    feat: add /etc/hosts for local network

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -1,5 +1,18 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   networking.networkmanager.enable = true;
+  networking.hosts = {
+    "172.19.20.11" = [ "proxmox.local" ];
+    "172.19.20.10" = [ "truenas.local" ];
+    "172.19.20.227" = [
+      "homelab.local"
+      "immich.homelab.local"
+    ];
+  };
 }
